1fa0b10ecfd5b4cee8a6c9a71de2e6b17cb1e00d
MaryT17 <tmary11996@gmail.com>
2024-06-30 23:56:39 +0800

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,8 @@
+
+// import user from "../json/user.json";
+
+import { Profile } from "./Profile/Profile";
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <Profile/>
 };
